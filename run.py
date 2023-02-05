1f00615667cf656d2de7d94a702e6713fcbb2e74
Levi Ob <commonnightingale@pm.me>
2023-02-05 12:30:35 -0800

    Complete task 2, load data into a pandas dataframe, reduce runtime by 50%

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import csv
+import pandas as pd
 
 
 def run():
@@ -11,51 +11,25 @@ def run():
     #  * Column 5 - water_temperature
     #  * Column 6 - wind_speed
 
-    # COLUMN_NAMES = [
-    #     "time",
-    #     "humidity",
-    #     "salinity",
-    #     "air_temperature",
-    #     "water_temperature",
-    #     "wind_speed"
-    # ]
+    COLUMN_NAMES = [
+        "time",
+        "humidity",
+        "salinity",
+        "air_temperature",
+        "water_temperature",
+        "wind_speed",
+    ]
 
-    # Load data from a local CSV file into a nested array
-    data = []
-    with open("data.csv") as csvdata:
-        csv_reader = csv.reader(csvdata)
-        for row in csv_reader:
-            data.append(row)
+    # Load data from CSV into dataframe
+    df = pd.read_csv("data.csv", names=COLUMN_NAMES, parse_dates=["time"])
 
-    # Transpose the nested array
-    transposed = list(map(list, zip(*data)))
-
-    # Remove NaN values and cast to float
-    for col_idx, col_data in enumerate(transposed):
-        # Do not cast time column to float
-        if col_idx == 0:
-            continue
-        transposed[col_idx] = [float(x) for x in col_data if not math.isnan(float(x))]
-
-    def column_average(col_num):
-        """Takes 1-indexed column number, returns the average of that column"""
-
-        return sum(transposed[col_num - 1]) / len(transposed[col_num - 1])
-
-    # Calculate the average of each column
-    col_2_avg = column_average(2)
-    col_3_avg = column_average(3)
-    col_4_avg = column_average(4)
-    col_5_avg = column_average(5)
-    col_6_avg = column_average(6)
-
-    # Return the averages of each column
+    # Return the means
     return {
-        "humidity": col_2_avg,
-        "salinity": col_3_avg,
-        "air_temperature": col_4_avg,
-        "water_temperature": col_5_avg,
-        "wind_speed": col_6_avg,
+        "humidity": df.humidity.mean(),
+        "salinity": df.salinity.mean(),
+        "air_temperature": df.air_temperature.mean(),
+        "water_temperature": df.water_temperature.mean(),
+        "wind_speed": df.wind_speed.mean(),
     }
 
 
